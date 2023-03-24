3f7968102d78d3d94407ae8acdaeea0057b8db75
masoud saed <masoudsaed918@gmail.com>
2023-03-24 21:37:23 +0000

    Added two header layout with feutuers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0 ">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+91 8264954234">
+                  +91 8264954234
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Dev Corner</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link>
+                    <img src="/" alt="" />
+                    <p></p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt="" />
+                    <p></p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt="" />
+                    <p></p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt="" />
+                    <p></p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
