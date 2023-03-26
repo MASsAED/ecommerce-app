ba1cb34e0f9ee9fcd01d727a7df163ca6462d205
masoud saed <masoudsaed918@gmail.com>
2023-03-26 00:36:25 +0000

    Footer and header Done!

--- src/components/Header.js
+++ src/components/Header.js
@@ -92,7 +92,7 @@ const Header = () => {
             <div className="col-12">
               <div className="menu-bottom d-flex align-items-center gap-30">
                 <div>
-                  <div class="dropdown">
+                  <div className="dropdown">
                     <button
                       className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
                       type="button"
